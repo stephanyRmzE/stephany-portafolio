42ef73f48a5b243666b9f886941004d57f68417b
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-03-30 12:08:42 -0700

    works filter works

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -9,29 +9,57 @@ import './Work.scss'
 const Work = () => {
   const [animateCard, setAnimateCard] = useState({ y:0, opacity:1})
   const [works, setWorks] = useState([])
+  const [filterWork, setFilterWork] = useState([])
+  const [activeFilter, setActiveFilter] = useState('All')
 
   useEffect(() => {
     const query = '*[_type == "works"]';
 
-    client.fetch(query).then((data)=>setWorks(data))
+    client.fetch(query).then((data)=>{
+      setWorks(data)
+      setFilterWork(data)
+    })
   }, [])
+
+  const handleWorkFilter = (item) => {
+
+    setActiveFilter(item);
+    setAnimateCard([{y:100, opacity: 0}])
+
+    setTimeout(() => {
+      setAnimateCard([{y:0, opacity: 1}])
+      if (item === 'All') {
+        setFilterWork(works)
+      } else{
+        setFilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+    },500);
+  }
+
 
   return (
     <>
       <h2 className="head-text">Crafting the <span> Future </span>of the Web: <span><br /> Stephany's </span>Portafolio</h2>
 
       <div className="app__work-filter">
-
+        {['React Js', 'Next Js', 'Rails', 'All'].map((item, index) =>(
+          <div
+            key = {index}
+            onClick = {() => handleWorkFilter(item)}
+            className = {`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
       </div>
 
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5}}
         className= 'app__work-portafolio'
-
       >
 
-        {works.map((work, index) =>(
+        {filterWork.map((work, index) =>(
           <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.title} />
